bd512b4ff69bd548f9faee1b98cdf63349f266f2
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2023-11-07 05:08:13 +0600

    added Login section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import { Link } from "react-router-dom";
+
+
+import { useContext } from "react";
+
+import Swal from "sweetalert2";
+import { AuthContext } from "../Provider/AuthProvider";
+const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
+    const navItems = (
+        <>
+            <li>
+                <Link className=" font-bold" to="/">Home</Link>
+            </li>
+            <li>
+                
+            </li>
+            {user && <>  <li>
+                {" "}
+                
+            </li>
+                <li>
+                    
+                </li> </>}
+            <li>
+
+            </li>
+        </>
+    );
+    const handleLogOut = () => {
+        logout()
+            .then(() => {
+                Swal.fire({
+                    title: 'success!',
+                    text: 'Logout Succesfull',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                })
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
+    return (
+        <div className="navbar bg-base-200 h-28">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+
+                        {navItems}
+                    </ul>
+                </div>
+
+
+
+
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navItems}
+                </ul>
+            </div>
+
+            <div className="navbar-end">
+                <label className=" mr-5">
+                    {user && <div className="tooltip tooltip-bottom" data-tip={user?.displayName}>
+                        <img style={{ width: "50px" }} className="w-50 rounded-full circle" src={user?.photoURL} />
+                    </div>}
+                </label>
+                {user ? <button onClick={handleLogOut} className="btn">LogOut</button>
+                    : <Link to="/login" className="btn">Login</Link>}
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import AuthProvider from './Provider/AuthProvider'
+import { RouterProvider } from 'react-router-dom'
+import router from './Router/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+  <div className="max-w-7xl mx-auto overflow-hidden">
+    <React.StrictMode>
+      <Toaster />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+
+    </React.StrictMode>
+  </div>
+
 )
