f3db0bdc07600f82b629fff5107415544b1af2f1
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2023-11-08 03:55:41 +0600

    edit on subjects

--- src/components/Subject.jsx
+++ src/components/Subject.jsx
@@ -12,18 +12,19 @@ const Subject = () => {
         data-aos-duration="1000"
         >
           <figure>
-            <img
-              src="https://img.freepik.com/free-photo/closeup-shot-small-toy-car-wooden-train-track_181624-32510.jpg?w=1060&t=st=1684590060~exp=1684590660~hmac=10bb9ce2efba4753482ddf08a1692a907dddc48beecf7a69d86f931ae422eeb8"
+            <img className="h-64 w-full"
+              src="https://collegerealitycheck.com/wp-content/uploads/physics-major-1988419205-1024x768.jpg"
               alt="Shoes"
             />
           </figure>
           <div className="card-body flex">
             <div className="text-center">
-              <h2 className="font-extrabold text-center text-2xl">Train Set</h2>
-              <p>Price: $450</p>
+              <h2 className="font-extrabold text-center text-2xl">Physics</h2>
+              <p>Physics is the natural science of matter, involving the study of matter, its fundamental constituents, its motion and behavior through space and time .
+</p>
             </div>
 
-            <button className="btn btn-primary ">Buy Now</button>
+            <button className="btn btn-primary ">View Notes</button>
           </div>
         </div>
         <div className="card card-compact w-96 bg-base-100 shadow-xl mt-5 "
@@ -31,18 +32,18 @@ const Subject = () => {
           data-aos-duration="1000"
         >
           <figure>
-            <img
-              src="https://img.freepik.com/free-photo/so-little-so-big-boy-child-looks-brave-driving-his-toy-outlander_1304-2723.jpg?w=996&t=st=1684356770~exp=1684357370~hmac=2c7e8e8b211ae7155ec8a28acd7ff4de0958517a9f374eaa779f0e92bf4e466a"
+            <img className="h-64 w-full"
+              src="https://img.freepik.com/premium-photo/investigations-defunct-chemistry-facility-laboratory-chemistry-instruction_410516-4221.jpg"
               alt="Shoes"
             />
           </figure>
           <div className="card-body flex">
             <div className="text-center">
-              <h2 className="font-extrabold text-center text-2xl">Outlander</h2>
-              <p>Price: $750</p>
+              <h2 className="font-extrabold text-center text-2xl">Chemistry</h2>
+              <p>Chemistry is a branch of natural science that deals principally with the properties of substances, the changes they undergo.</p>
             </div>
 
-            <button className="btn btn-primary ">Buy Now</button>
+            <button className="btn btn-primary ">View Notes</button>
           </div>
         </div>
         <div className="card card-compact w-96 bg-base-100 shadow-xl mt-5 "
@@ -50,18 +51,18 @@ const Subject = () => {
           data-aos-duration="1000"
         >
           <figure>
-            <img
-              src="https://img.freepik.com/free-photo/little-baby-girl-playing-car_155003-4389.jpg?w=996&t=st=1684356866~exp=1684357466~hmac=59edb365d7e8056c69b620443abe0f615e67686b233e53078ae6794124bec04d"
+            <img className="h-64 w-full"
+              src="https://images.squarespace-cdn.com/content/v1/5fce63270356d927d7eecdbd/033e9988-2ac8-4cb9-8b9f-5bf05fb22dcb/gff.jpg"
               alt="Shoes"
             />
           </figure>
           <div className="card-body flex">
             <div className="text-center">
-              <h2 className="font-extrabold text-center text-2xl">Volkswagen Beetle</h2>
-              <p>Price: $650</p>
+              <h2 className="font-extrabold text-center text-2xl">software engineering</h2>
+              <p>Software engineering is the branch of computer science that deals with the design, development, testing, and maintenance of software applications.</p>
             </div>
 
-            <button className="btn btn-primary ">Buy Now</button>
+            <button className="btn btn-primary ">View Notes</button>
           </div>
         </div>
       </div>
